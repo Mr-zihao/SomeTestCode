9995c0142de40d7f8c91a3119b7c64c007006ad0
littleeye <upc.zihaolu@gmail.com>
2018-07-06 22:08:45 +0800

    Update TensorflowReadPBfile.py

--- TensorflowReadPBfile.py
+++ TensorflowReadPBfile.py
@@ -6,3 +6,14 @@ with tf.Session() as sess:
     graph_def.ParseFromString(f.read())
     tf.import_graph_def(graph_def=graph_def,name='')
   print(tf.get_default_graph().get_operations())
+  
+  
+# import tensorflow as tf
+# with tf.Graph().as_default():
+#     output_graph_def=tf.GraphDef()
+#     output_graph_path='D:/face/inception_dec_2015/tensorflow_inception_graph.pb'
+#     with open(output_graph_path,"rb") as f:
+#         output_graph_def.ParseFromString(f.read())
+#         _=tf.import_graph_def(output_graph_def,name='')
+#     with tf.Session() as sess:
+#         print(sess.graph.get_tensor_by_name('input:0'))
